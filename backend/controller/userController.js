6ec380e283bec05ea313c5b4669d1f7883f11a9b
Henrietta Zarvou <henriettazarvou@gmail.com>
2022-03-16 19:20:14 +0200

    authenticate user

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -37,6 +37,19 @@ const registerUser = asyncHandler(async (req, res) => {
 });
 
 const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid credentials");
+  }
+
   res.json({ message: "Login User" });
 });
 
