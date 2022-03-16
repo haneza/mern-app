04dad4293462034b7ab1c8acc2cb9e04bd9c90f3
Henrietta Zarvou <henriettazarvou@gmail.com>
2022-03-16 14:38:43 +0200

    controller added, refactoring routes

---
+++ backend/controller/goalController.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const getGoals = (req, res) => {
+  res.status(200).json({ message: "Get goals" });
+};
+const setGoal = (req, res) => {
+  res.status(200).json({ message: "Set goals" });
+};
+const updateGoal = (req, res) => {
+  res.status(200).json({ message: `Update goal ${req.params.id}` });
+};
+const deleteGoal = (req, res) => {
+  res.status(200).json({ message: `Delete goal ${req.params.id}` });
+};
+
+module.exports = {
+  getGoals,
+  setGoal,
+  updateGoal,
+  deleteGoal,
+};
