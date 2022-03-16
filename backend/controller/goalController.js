7c57d204255623cea8f8d26cf191d93738163e2a
Henrietta Zarvou <henriettazarvou@gmail.com>
2022-03-16 15:09:30 +0200

    middleware added

--- backend/controller/goalController.js
+++ backend/controller/goalController.js
@@ -3,12 +3,16 @@
 const getGoals = (req, res) => {
   res.status(200).json({ message: "Get goals" });
 };
+
 const setGoal = (req, res) => {
+  console.log(req.body);
   res.status(200).json({ message: "Set goals" });
 };
+
 const updateGoal = (req, res) => {
   res.status(200).json({ message: `Update goal ${req.params.id}` });
 };
+
 const deleteGoal = (req, res) => {
   res.status(200).json({ message: `Delete goal ${req.params.id}` });
 };
